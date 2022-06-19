b2555355e40c986f2635ecc21e28303061b6113b
Saiful Bashar <robal.bashar59@gmail.com>
2022-06-19 18:54:38 +0600

    format and tree sitter

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,11 +9,17 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
+<<<<<<< Updated upstream
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
     -- diagnostics.flake8
+=======
+	debug = true,
+	sources = {
+		formatting.prettier
+>>>>>>> Stashed changes
 	},
 })
